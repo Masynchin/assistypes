ab5a6e4640298bd8659c398ca6e80a5679787721
Masynchin <47028153+Masynchin@users.noreply.github.com>
2020-08-07 01:52:29 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='my_types',
+      version='1.0',
+      description='My special types',
+      author='Masynchin',
+      author_email='banzur.badurov@gmail.com',
+      url='https://github.com/Masynchin/my-types-python',
+      packages=['my_types'],
+      )
