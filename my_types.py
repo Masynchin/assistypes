3d3b3eed53073a66941174dc8e65f91d630d500e
Masynchin <47028153+Masynchin@users.noreply.github.com>
2020-08-07 17:29:29 +0300

    Change ranges
    1) Убрал MutableRange
    2) FloatRange теперь умеет в обратный порядок, первое число теперь тоже float
    3) RoundRange теперь RoundedRange + всё, что получил FloatRange + теперь в выводе пишет ещё и rounded
    4) Убрана функция mul

--- my_types.py
+++ my_types.py
@@ -41,77 +41,7 @@ class StructuredList(list):
 
 # RANGES
 
-class MutableRange:
-	def __init__(self, *args):
-		if (ln := len(args)) > 3 or ln == 0:
-			raise ValueError('the class requires one, two or three arguments')
-		elif ln == 1:
-			self.start = 0
-			self.stop = args[0]
-			self.step = 1
-		elif ln == 2:
-			self.start = args[0]
-			self.stop = args[1]
-			self.step = 1
-		elif ln == 3:
-			self.start, self.stop, self.step = args
-		
-		if self.step == 0:
-			raise ValueError("step can't be equal 0")
-		if (self.stop - self.start) / self.step < 0:
-			raise ValueError('infinite range')
-
-		self.i = self.start
-
-	def __iter__(self):
-		return self.range()
-	
-	def range(self):
-		while self.i < self.stop:
-			yield self.i
-			self.i += self.step
-
-	def __repr__(self):
-		return f'{self.__class__.__name__}(start={self.start}, stop={self.stop}, step={self.step})'
-
-	def change_start(self, new_start):
-		self.start = new_start
-
-		if (self.stop - self.start) / self.step < 0:
-			raise ValueError('infinite range')
-
-	def change_step(self, new_step):
-		self.step = new_step
-
-		if (self.stop - self.start) / self.step < 0:
-			raise ValueError('infinite range')
-
-	def change_stop(self, new_stop):
-		self.stop = new_stop
-
-		if (self.stop - self.start) / self.step < 0:
-			raise ValueError('infinite range')
-
-	def add_to_start(self, diff):
-		self.start += diff
-
-		if (self.stop - self.start) / self.step < 0:
-			raise ValueError('infinite range')
-
-	def add_to_step(self, diff):
-		self.step += diff
-
-		if (self.stop - self.start) / self.step < 0:
-			raise ValueError('infinite range')
-
-	def add_to_stop(self, diff):
-		self.stop += diff
-
-		if (self.stop - self.start) / self.step < 0:
-			raise ValueError('infinite range')
-
-
-class FloatRange(MutableRange):
+class FloatRange:
 	def __init__(self, *args):
 		if (ln := len(args)) > 3 or ln == 0:
 			raise ValueError('need at least one argument')
@@ -131,32 +61,47 @@ class FloatRange(MutableRange):
 		if (self.stop - self.start) / self.step < 0:
 			raise ValueError('infinite range')
 
+		self.start = float(self.start)
 		self.i = self.start
 		self.step = float(self.step)
 
-	# метод __iter__ определён в родительском классе
-	# метод __repr__ определён в родительском классе
+	def __iter__(self):
+		return self.range()
+	
+	def range(self):
+		if self.start < self.stop:
+			while self.i < self.stop:
+				yield self.i
+				self.i += self.step
+		else:
+			while self.i > self.stop:
+				yield self.i
+				self.i += self.step
+
+	def __repr__(self):
+		return f'FloatRange(start={self.start}, stop={self.stop}, step={self.step})'
 
 
-class RoundRange(FloatRange):
+class RoundedRange(FloatRange):
 	def __init__(self, *args):
 		super().__init__(*args)
-		self.float_len = len(str(self.step).split('.')[1])
+		self.float_len = len(str(self.step).split('.')[-1])
+
+	def __iter__(self):
+		return self.range()
 
 	def range(self):
-		while self.i < self.stop:
-			yield self.i
-			self.i += self.step
-			self.i = round(self.i, self.float_len)
+		if self.start < self.stop:
+			while self.i < self.stop:
+				yield self.i
+				self.i += self.step
+				self.i = round(self.i, self.float_len)
+		else:
+			while self.i > self.stop:
+				yield self.i
+				self.i += self.step
+				self.i = round(self.i, self.float_len)
 
-	# метод __iter__ определён в родительском классе
-	# метод __repr__ определён в родительском классе
-
-
-# OTHER FUNCTIONS
-
-def mul(seq):
-	result = 1
-	for n in seq:
-		result *= n
-	return result
+	def __repr__(self):
+		return (f'RoundRange(start={self.start}, stop={self.stop}, '
+				f'step={self.step}, rounded={self.float_len})')
